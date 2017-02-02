729fe3fc8cf8279a4abddab8171a6b1454e91d1c
Duckieeeeee <basketballfan_98@hotmail.com>
2017-02-02 13:22:48 -0700

    Starting Project

---
+++ Ball Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(-1.0f, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+void OnTriggerEnter(Collider other)
+{
+    if (other.tag == "Wall")
+    {
+        Time.timeScale = 0;
+    }
+}
+}
+
---
+++ Ball Dodge/Ball-Dodge/README.md
@@ -0,0 +1,9 @@
+# Ball-Dodge
+this game is great play me :)
+The greatest programmer of all time made this!!!!!!
+      #Im a God
+        Praise Me because im Godly
+        
+        
+                    <3
+                      Dusty J
